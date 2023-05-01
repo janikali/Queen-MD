fc05eb36263fb04519817bd9e52082fa31306f0e
janikali <132261138+janikali@users.noreply.github.com>
2023-05-01 15:40:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'UTJ1TDRiYUQ=',
+global.ownername = process.env.OWNER_NAME === undefined ? "janindu" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94740978159' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
